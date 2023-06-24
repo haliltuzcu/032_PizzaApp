f72575a3e5a4b5d9fe208dba90baf826b00ffd18
haliltuzcu <007canberk@gmail.com>
2023-06-24 13:14:57 +0300

    first commit

---
+++ pizza/views.py
@@ -0,0 +1,86 @@
+from django.shortcuts import render
+
+def home(request):
+    from .models import Pizza
+    pizzas = Pizza.objects.all()
+    context = {
+        'pizzas': pizzas
+    }
+    return render(request, 'home.html', context)
+
+
+# ------------------------------------
+# Class Based Views
+# ------------------------------------
+from django.contrib import messages
+from django.urls import reverse_lazy
+
+from .forms import (
+    Order, OrderForm
+)
+
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
+
+# ------------------------------
+# FixView
+# ------------------------------
+class FixView:
+    model = Order
+    form_class = OrderForm
+    success_url = reverse_lazy('order_list')
+
+
+# ------------------------------
+# Views
+# ------------------------------
+
+# List:
+class OrderListView(FixView, ListView):
+    template_name = 'pizza/order_list.html'
+    context_object_name = 'orders'
+    order = ['-id']
+
+
+# Create:
+class OrderCreateView(FixView, CreateView):
+    template_name = 'pizza/order_form.html'
+    context_object_name = 'form'
+    
+    
+    def post(self, request, *args, **kwargs):
+        # Mesaj göster:
+        messages.success(request, 'Kaydedildi.')
+        return super().post(request, *args, **kwargs)
+
+
+# Detail:
+class OrderDetailView(FixView, DetailView):
+    template_name = 'pizza/order_detail.html'
+    context_object_name = 'order'
+
+
+# Update:
+class OrderUpdateView(FixView, UpdateView):
+    template_name = 'pizza/order_form.html'
+    context_object_name = 'form'
+
+    def post(self, request, *args, **kwargs):
+        # Mesaj göster:
+        messages.success(request, 'Güncellendi.')
+        return super().post(request, *args, **kwargs)
+
+
+# Delete:
+class OrderDeleteView(FixView, DeleteView):
+    
+    # template dosyasına gitmeden direkt sil:
+    def get(self, request, *args, **kwargs):
+        # Mesaj göster:
+        messages.success(request, 'Silindi.')
+        return super().delete(request, *args, **kwargs)
